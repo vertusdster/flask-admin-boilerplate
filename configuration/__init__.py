fbb092158569114328ec8a08c4a744c93d2b549b
vertusdster <vertusdster@gmail.com>
2023-03-03 17:21:34 +0000

    init

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -6,18 +6,18 @@ import os
 app.secret_key = "ITSASECRET"
 
 #setting up mail
-app.config['MAIL_SERVER']='' #mail server
+app.config['MAIL_SERVER']='127.0.0.1' #mail server
 app.config['MAIL_PORT'] = 587 #mail port
-app.config['MAIL_USERNAME'] = '' #email
+app.config['MAIL_USERNAME'] = 'vertusd@gmail.com' #email
 app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD'] #password
 app.config['MAIL_USE_TLS'] = True #security type
 app.config['MAIL_USE_SSL'] = False #security type
 
 #database connection parameters
 connection_params = {
-    'user': '',
+    'user': '123',
     'password': os.environ['DB_PASSWORD'],
-    'host': '',
-    'port': 'port',
+    'host': '127.0.0.1',
+    'port': '33',
     'namespace': '',
 }
